cafbdaccb85fcbf123230d27554539c93ab632e3
luigiboy67 <shamgod98@gmail.com>
2024-03-02 15:29:14 -0600

    add plots and fixed the grids

--- server.R
+++ server.R
@@ -5,6 +5,15 @@ source("plots.R")
 # the shiny server
 server <- shinyServer(
   function(input, output, session) {
-    
+    output$rocket <- renderPlot(theme_rocket)
+    output$gameboy <- renderPlot(gameboy_theme)
+    output$advance <- renderPlot(gameboy_advance_theme)
+    output$hpbar <- renderPlot(pokemon_gghealth)
+    output$mystic <- renderPlot(p1)
+    output$valor <- renderPlot(p2)
+    output$instinct <- renderPlot(p3)
+    output$grids <- renderPlot(legrid)
+    output$effectiveness <- renderPlot(effectiveness)
+    output$types <- renderPlot(pokemon_types)
   }
 )

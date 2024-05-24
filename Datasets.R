ced346b5a87a3d97d372c5771e67e3ff1b6cae44
ShaMarcus Anders <shamgod98@gmail.com>
2024-05-23 19:33:50 -0500

    created data sets for pokemon

---
+++ Datasets.R
@@ -0,0 +1,20 @@
+kanto <- pokemon %>%
+  filter(generation == 1)
+
+johto <- pokemon %>%
+  filter(generation == 2)
+
+hoenn <- pokemon %>%
+  filter(generation == 3)
+
+sinnoh <- pokemon %>%
+  filter(generation == 4)
+
+unova <- pokemon %>%
+  filter(generation == 5)
+
+kalos <- pokemon %>%
+  filter(generation == 6)
+
+alola <- pokemon %>%
+  filter(generation == 7)

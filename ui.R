0a4f8067f54698365b3611e5ec8839cb875b53b0
ShaMarcus Anders <shamgod98@gmail.com>
2024-05-28 19:58:02 -0500

    changed to a navbar page

--- ui.R
+++ ui.R
@@ -3,30 +3,34 @@ source("packages.R")
 source("plots.R")
 
 # shiny app
-ui <- shinyUI(dashboardPage( title = "Pokemon App",
-  dashboardHeader(title = "Pokemon App", tags$li(class = "dropdown", tags$img(src = "gen-1/6.png"))),
-  dashboardSidebar(disable = TRUE),
-  dashboardBody(tabsetPanel(
-    tabPanel(title = "Team Rocket", 
-             plotOutput("rocket", height = 700)),
-    tabPanel(title =  "Gameboy", 
-             plotOutput("gameboy", height = 700)),
-    tabPanel(title =  "Gameboy Advance", 
-             plotOutput("advance", height = 700)),
-    tabPanel(title =  "Status/HP Bar", 
-             plotOutput("hpbar", height = 700)),
-    tabPanel(title =  "Team Mystic", 
-             plotOutput("mystic", height = 700)),
-    tabPanel(title =  "Team Valor", 
-             plotOutput("valor", height = 700)),
-    tabPanel(title =  "Team Instinct", 
-             plotOutput("instinct", height = 700)),
-    tabPanel(title =  "Pokemon Grid", 
-             plotOutput("grids", height = 700)),
-    tabPanel(title =  "Effective Table", 
-             plotOutput("effectiveness", height = 700)),
-    tabPanel(title =  "Pokemon Types", 
-             plotOutput("types", height = 700))
-  ))
-)
-)
+ui <- shinyUI(
+  
+  navbarPage("Pokemon App",
+             tabPanel("Example Page",
+                      mainPanel(  
+                        tabsetPanel(
+                          tabPanel(title = "Team Rocket",
+                                 plotOutput("rocket", height = 700)),
+                          tabPanel(title =  "Gameboy",
+                                 plotOutput("gameboy", height = 700)),
+                          tabPanel(title =  "Gameboy Advance",
+                                 plotOutput("advance", height = 700)),
+                          tabPanel(title =  "Status/HP Bar",
+                                 plotOutput("hpbar", height = 700)),
+                          tabPanel(title =  "Team Mystic",
+                                 plotOutput("mystic", height = 700)),
+                          tabPanel(title =  "Team Valor",
+                                 plotOutput("valor", height = 700)),
+                          tabPanel(title =  "Team Instinct",
+                                 plotOutput("instinct", height = 700)),
+                          tabPanel(title =  "Pokemon Grid",
+                                 plotOutput("grids", height = 700)),
+                          tabPanel(title =  "Effective Table",
+                                 plotOutput("effectiveness", height = 700)),
+                          tabPanel(title =  "Pokemon Types",
+                                 plotOutput("types", height = 700)))
+                        )
+             ),
+             tabPanel("Generation Page")
+             )
+  )

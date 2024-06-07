a0c1c63a953b246d724e3062f5161a83a09afaec
ShaMarcus Anders <shamgod98@gmail.com>
2024-06-06 19:24:46 -0500

    created plots for the generation page

--- ui.R
+++ ui.R
@@ -7,7 +7,8 @@ ui <- shinyUI(
   
   navbarPage("Pokemon App",
              tabPanel("Example Page",
-                      mainPanel(  
+                      mainPanel(
+                        width = 12,  
                         tabsetPanel(
                           tabPanel(title = "Team Rocket",
                                  plotOutput("rocket", height = 700)),
@@ -31,6 +32,47 @@ ui <- shinyUI(
                                  plotOutput("types", height = 700)))
                         )
              ),
-             tabPanel("Generation Page")
+             tabPanel("Generation Page",
+                      mainPanel(
+                        width = 12,
+                        tabsetPanel(
+                          tabPanel(
+                            title = "Generation 1",
+                            plotOutput("gen1", height = 700)
+                          ),
+                          tabPanel(
+                            title = "Generation 2",
+                            plotOutput("gen2", height = 700)
+                          ),
+                          tabPanel(
+                            title = "Generation 3",
+                            plotOutput("gen3", height = 700)
+                          ),
+                          tabPanel(
+                            title = "Generation 4",
+                            plotOutput("gen4", height = 700)
+                          ),
+                          tabPanel(
+                            title = "Generation 5",
+                            plotOutput("gen5", height = 700)
+                          ),
+                          tabPanel(
+                            title = "Generation 6",
+                            plotOutput("gen6", height = 700)
+                          ),
+                          tabPanel(
+                            title = "Generation 7",
+                            plotOutput("gen7", height = 700)
+                          ),
+                          tabPanel(
+                            title = "Generation 8",
+                            plotOutput("gen8", height = 700)
+                          ),
+                          tabPanel(
+                            title = "Generation 9",
+                            plotOutput("gen9", height = 700)
+                          )
+                        )
+                      ))
              )
   )
